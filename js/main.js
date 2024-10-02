e0c61b77ed7348641e64b899c1d85660f21b4c6d
Nancy Kama <70664950+nancykama@users.noreply.github.com>
2024-10-01 21:32:50 -0700

    script

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
- window.addEventListener('scroll', function() {
-    const footer = document.getElementById('footer');
-    const bodyHeight = document.body.offsetHeight;
-    const windowHeight = window.innerHeight;
-    const scrollY = window.scrollY;
 
-    // Check if user scrolled to the bottom
-    if (windowHeight + scrollY >= bodyHeight) {
-        footer.classList.add('active');
-    } else {
-        footer.classList.remove('active');
-     }
-});
+        window.addEventListener('scroll', function() {
+            const footer = document.getElementById('footer');
+            const bodyHeight = document.body.offsetHeight;
+            const windowHeight = window.innerHeight;
+            const scrollY = window.scrollY;
+
+            // Check if user scrolled to the bottom
+            if (windowHeight + scrollY >= bodyHeight) {
+                footer.classList.add('active');
+            } else {
+                footer.classList.remove('active');
+         }
+        }); 
