b3ae1dd6382f153c045a39c876986943e5377516
Nancy Kama <70664950+nancykama@users.noreply.github.com>
2024-10-01 20:20:09 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 window.addEventListener('scroll', function() {
     const footer = document.getElementById('footer');
-    // Check how far the user has scrolled down
-    if (window.scrollY > 200) { // Change 200 to whatever scroll distance you want
-        footer.classList.add('show'); // Add the show class to make it visible
+    const bodyHeight = document.body.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+
+    // Check if user scrolled to the bottom
+    if (windowHeight + scrollY >= bodyHeight) {
+        footer.classList.add('active');
     } else {
-        footer.classList.remove('show'); // Remove the class to hide it
+        footer.classList.remove('active');
     }
 });
